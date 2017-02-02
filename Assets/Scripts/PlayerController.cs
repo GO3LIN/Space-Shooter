649aa4593c46d4b21c21b0b1bad9ff996f80ebdb
GO3LIN <benmohamehdi@gmail.com>
2017-02-02 16:52:25 +0000

    First commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary {
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Boundary boundary;
+	public float tilt;
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate = 0.25F;
+
+	private Rigidbody player;
+	private AudioSource sound;
+	private float nextFire = 0.0F;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<Rigidbody> ();
+		sound = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update(){
+		if (Input.GetKey(KeyCode.Space) && Time.time > nextFire 
+			|| Input.GetKey(KeyCode.Space) && Input.GetKeyDown (KeyCode.LeftArrow) && Time.time > nextFire){
+			nextFire = Time.time + fireRate;	
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+			sound.Play ();
+		}
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+		player.velocity = movement * speed;
+		player.position = new Vector3 (
+			Mathf.Clamp(player.position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp(player.position.z, boundary.zMin, boundary.zMax)
+		);
+		player.rotation = Quaternion.Euler (0.0f, 0.0f, player.velocity.x * tilt);
+		
+	}
+}
---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+
+	public float tumble;
+
+	private Rigidbody asteroid;
+
+	// Use this for initialization
+	void Start () {
+		asteroid = GetComponent<Rigidbody> ();
+		asteroid.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
